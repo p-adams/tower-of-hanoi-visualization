899e2e83c781b9f9bfca4464df8d6c18000e4f5f
User <paadams@nmu.edu>
2017-06-18 18:27:04 -0400

    updated project

---
+++ components/PuzzleCanvas.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        puzzle canvas {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'woof'
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,18 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <PuzzleCanvas/>
+
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
+import PuzzleCanvas from '~components/PuzzleCanvas.vue'
 
 export default {
   components: {
-    Logo
+    PuzzleCanvas
   }
 }
 </script>
