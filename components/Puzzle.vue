43af0f812311e2c95708e2a7d9b89372eaf104a3
User <paadams@nmu.edu>
2017-06-27 13:49:33 -0400

    updated data

--- components/Puzzle.vue
+++ components/Puzzle.vue
@@ -16,7 +16,80 @@
 export default {
   data () {
     return {
-      msg: 'woof'
+      moves: 0,
+      isEmpty: false,
+      selectedDisk: '',
+      fromPole: [],
+      toPole: [],
+      disk1: {id: 1, isTop: false, color: 'white', width: '50px'},
+      disk2: {id: 2, isTop: false, color: '#41B883', width: '100px'},
+      disk3: {id: 3, isTop: false, color: '#35495E', width: '150px'},
+      src: [],
+      aux: [],
+      dest: []
+    }
+  },
+  created () {
+    this.stackDisks()
+  },
+  methods: {
+    stackDisks () {
+      this.src.unshift(this.disk3)
+      this.src.unshift(this.disk2)
+      this.disk1.isTop = true
+      this.src.unshift(this.disk1)
+    },
+    restart () {
+      this.src = []
+      this.stackDisks()
+      this.aux = []
+      this.dest = []
+      this.moves = 0
+    },
+    moveDisk (pole, disk) {
+      [this.selectedDisk, this.fromPole] = [disk, pole]
+    },
+    setDisk (toPole) {
+      this.toPole = toPole
+      if (this.selectedDisk.isTop && this.isLegal()) {
+        this.fromPole.map(theDisk => {
+          if (theDisk.id === this.selectedDisk.id) {
+            this.fromPole.shift()
+            toPole.unshift(theDisk)
+            this.setTop
+          }
+        })
+        this.moves++
+      } else {
+        if (!this.selectedDisk.isTop) alert('cant move bottom disk')
+        if (!this.isLegal()) alert('cant move larger disk on smaller disk')
+      }
+    },
+    setTop () {
+      this.toPole.forEach(disk => {
+        if (disk.id !== this.toPole[0].id) {
+          disk.isTop = false
+        }
+        this.toPole[0].isTop = true
+      })
+      this.fromPole.forEach(disk => {
+        if (disk.id !== this.fromPole[0].id) {
+          disk.isTop = false
+        }
+        this.fromPole[0].isTop = true
+      })
+    },
+    isSmaller () {
+      let sm = false
+      this.toPole.map(disk => {
+        if (this.selectedDisk.id < disk.id) {
+          sm = true
+        }
+      })
+      return sm
+    },
+    isLegal () {
+      return this.toPole.length === 0 || this.isSmaller()
     }
   }
 }
